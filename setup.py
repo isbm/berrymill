eff372f9b5e305f319e423cc4a3ce8a467228b54
Bo Maryniuk <bo@maryniuk.net>
2023-07-19 19:00:43 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+from distutils.core import setup
+
+setup(name='Berrymill',
+      version='0.1',
+      description='Appliance builder',
+      author='Bo Maryniuk',
+      author_email='bo@maryniuk.net',
+      url='https://github.com/isbm/berrymill',
+      packages=['berry_mill'],
+     )
