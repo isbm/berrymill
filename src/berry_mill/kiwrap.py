df0b7b078bc923a1259fc49a22fdfb6f05d6d7a9
Andre Barthel <Andre.Barthel@elektrobit.com>
2023-08-17 01:00:04 +0200

    code improvements

--- src/berry_mill/kiwrap.py
+++ src/berry_mill/kiwrap.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
 from typing import List, Set
+from typing import TypedDict
+from typing_extensions import Unpack
 from lxml import etree
 import os
 import sys
@@ -8,35 +10,51 @@ import subprocess
 import shutil
 import tempfile
 import requests
+import platform
 
 from urllib.parse import urlparse
 from typing import Dict
+
+
+class KiwiParams(TypedDict):
+    """
+    Dictionary for Kiwi Parameters
+
+    Attributes:
+
+        profile (str, Optional): select profile for images that makes use of it.
+        box_memory (str, Default: 8G): specify main memory to use for the QEMU VM (box).
+        debug (bool, Default: False): run in debug mode. This means the box VM stays open and the kiwi log level is set to debug(10).
+        clean (bool, Default: False): cleanup previous build results prior build.
+        cross (bool, Default: False): cross image build on x86_64 to aarch64 target.
+        cpu (str, Optional): cpu to use for the QEMU VM (box)
+        local (bool, Default: False): run build process locally on this machine. Requires sudo setup and installed KIWI toolchain.
+        target_dir (str, Default:/var/tmp/IMAGE_NAME[.PROFILE_NAME]): store image results in given dirpath.
+    """
+    profile: str
+    box_memory: str
+    debug: bool
+    clean: bool
+    cross: bool
+    cpu: str
+    local: bool
+    target_dir: str
 
 
 class KiwiBuilder:
     """
     Kiwi wrapper for appliance builder.
     """
-    def __init__(self, pth:str, descr:str, profile:str="", box_memory:str="", debug:bool=False, clean:bool=False, cross:bool=False, cpu:str="", arch:str="x86_64", local:bool=False, target_dir:str="/tmp"):
+    # TODO handle TypeError
+    def __init__(self, pth:str, descr:str, **kw: Unpack[KiwiParams]):
         self._repos:Dict[str, Dict[str, str]] = {}
         self._appliance_path:str = pth
         self._appliance_descr:str = descr
-        self._local:bool = local
-        self._profile:str = profile
-        self._box_mem:str = box_memory
-        self._debug:bool = debug
-        self._clean:bool = clean
-        self._cpu:str = cpu
-        self._cross:bool = cross
-        self._arch:str = arch
+        self._params:Dict[KiwiParams] = kw
 
-        if target_dir.endswith("/"):
-            target_dir = target_dir.removesuffix("/")
-        if target_dir.find(" ") != -1:
-            print("ERROR: target directory contains at least one space ' '")
-            sys.exit(1)
+        if "target_dir" in self._params:
+            self._params["target_dir"] = self._params.get("target_dir").rstrip("/")
 
-        self._target_dir = target_dir
         self._tmpdir = tempfile.mkdtemp(prefix="berrymill-keys-", dir="/tmp")
 
     def add_repo(self, reponame:str, repodata:Dict[str, str]) -> KiwiBuilder:
@@ -79,7 +97,6 @@ class KiwiBuilder:
         Run builder. It supposed to be already within that directory (os.chdir).
         Directory is changed by the parent caller of the KiwiBuilder class.
         """
-
         print("Using appliance \"{}\" located at \"{}\"".format(self._appliance_descr, self._appliance_path))
         # options that are solely accepted by kiwi-ng
         kiwi_options = []
@@ -87,86 +104,78 @@ class KiwiBuilder:
         # options, solely accepted by box-build plugin
         box_options:List[str] = ["--box","ubuntu"]
 
-        if self._debug:
+        if self._params.get("debug", False):
             kiwi_options.append("--debug")
             box_options.append("--box-debug")
         
-        if len(self._cpu) > 0:
-            box_options.insert(0, "--cpu")
-            box_options.insert(1, self._cpu)
+        if "cpu" in self._params:
+            box_options = ["--cpu", self._cpu] + box_options
         
-        if len(self._box_mem) > 0:
-            box_options.append("--box-memory")
-            box_options.append(self._box_mem)
+        if "box_memory" in self._params:
+            box_options += ["--box-memory", self._params.get("box_memory")]
         
-        if self._arch == "aarch64":
-            box_options.append("--machine")
-            box_options.append("virt")
+        if platform.machine() == "aarch64":
+            box_options += ["--machine", "virt"]
         
-        if self._cross and self._arch == "x86_64":
-            box_options.append("--aarch64")
-            box_options.append("--cpu")
-            box_options.append("cortex-a57")
-            box_options.append("--machine")
-            box_options.append("virt")
-            box_options.append("--no-accel")
-
-            kiwi_options.append("--target-arch")
-            kiwi_options.append("aarch64")
+        if self._params.get("cross", False) and platform.machine() == "x84_64":
+            box_options += ["--aarch64", "--cpu", "cortex-a57", "--machine", "virt", "--no-accel"]
+            kiwi_options += ["--target-arch", "aarch64"]
 
 
         try:
             config_tree = etree.parse(f"{self._appliance_path}/{self._appliance_descr}")
-        except Exception as exc:
-            print("ERROR: while trying to parse image description")
+        except Exception as err:
+            print("ERROR: Failure {} while parsing appliance description".format(err))
             sys.exit(1)
         
         try: 
             image_name:list = config_tree.xpath("//image/@name")
-        except Exception as exc:
-            print("ERROR: while trying to extract image name")
+        except Exception as err:
+            print("ERROR: Failure {} while trying to extract image name", err)
             sys.exit(1)
         
-        self._target_dir += f"/{image_name[0]}"
+        target_dir = self._params.get("target_dir", "/var/tmp")
+        target_dir += f"/{image_name[0]}"
 
-        if len(self._profile) > 0:
+        if "profile" in self._params:
             profiles = config_tree.xpath("//profile/@name")
             # TODO handle multiple profiles exist problem
             print(profiles)
         
-        if self._clean:
-            clean_target = self._target_dir
-            if len(self._profile) > 0:
-                clean_target += f".{self._profile}"
-            if self._local:
-                subprocess.run(["sudo", "rm", "-rf", clean_target])
+        if self._params.get("clean", False):
+            clean_target = target_dir
+            if "profile" in self._params:
+                clean_target += f".{self._params.get('profile')}"
+            if self._params.get("local", False):
+                shutil.rmtree(clean_target)
         
         
         repo_build_options:List[str] = ["--ignore-repos"]
-        alias = "repo1"
-        for repo in self._repos:
-            repo_build_options.append("--add-repo")
-            # source,type,alias,priority,imageinclude,package_gpgcheck,{signing_keys},components,distribution,repo_gpgcheck
-            repo_build_options.append(f"{self._repos[repo].get('url')},apt-deb,{repo},,,,{{{self._repos[repo].get('key')}}},{self._repos[repo].get('components').replace(',',' ')},{self._repos[repo].get('name')},false")
-            alias += "1"
+        for repo_name in self._repos.keys():
+            repo_content = self._repos.get(repo_name)
+            components = repo_content.get("components").split(',')
+            for component in components:
+                repo_build_options.append("--add-repo")
+                # syntax of --add-repo value:
+                # source,type,alias,priority,imageinclude,package_gpgcheck,{signing_keys},component,distribution,repo_gpgcheck
+                repo_build_options += [f"{repo_content.get('url')},{repo_content.get('type')},{repo_name},,,,{{{repo_content.get('key')}}},{component if component != '/' else ''},{repo_content.get('name')},false"]
         
-        if self._local:
+        if self._params.get("local", False):
             try:
-                command = ["kiwi-ng"] + kiwi_options + ["system", "build"] + ["--description"] + [self._appliance_path] + ["--target-dir"] + [self._target_dir] + repo_build_options
-
+                command = ["kiwi-ng"] + kiwi_options + ["system", "build", "--description"] + [self._appliance_path] + ["--target-dir", target_dir] + repo_build_options
+                # for debugging, no usage of print() to ensure better readability of insanely long kiwi command with no confusion of important ',' chars
+                # subprocess.run(["echo", "\""] + command + ["\""])
                 subprocess.run(command)
             except Exception as exc:
-                print(command)
-                print("ERROR: while calling kiwi in local mode")
+                print(exc)
                 sys.exit(1)     
         else:
             try:
-                command = ["kiwi-ng"]+ kiwi_options + ["system", "boxbuild"] + box_options + ["--"]+ ["--description"] + [self._appliance_path] + ["--target-dir"] + [self._target_dir] + repo_build_options
-                subprocess.run(["echo", "\""] + command + ["\""])
-                # subprocess.run(command)
+                command = ["kiwi-ng"]+ kiwi_options + ["system", "boxbuild"] + box_options + ["--", "--description"] + [self._appliance_path] + ["--target-dir", target_dir] + repo_build_options
+                # subprocess.run(["echo", "\""] + command + ["\""])
+                subprocess.run(command)
             except Exception as exc:
-                print(command)
-                print("ERROR: while calling kiwi in boxbuild mode")
+                print(exc)
                 sys.exit(1)
             
                 
