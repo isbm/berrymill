bc920baaea075011ab07f4c446fb6dbaa16e1173
Bo Maryniuk <bo@maryniuk.net>
2023-10-09 12:59:24 +0200

    Add UT scaffold for the image description

---
+++ test/test_imgdescr.py
@@ -0,0 +1,14 @@
+from berry_mill.imgdescr import ApplianceDescription
+import pytest
+
+
+class TestImgDescr_Packages:
+    """
+    Unit test suite for `<packages/>` aggregate.
+    """
+
+    def test_id_pkg_replace(self):
+        """
+        Replace `<packages/>`.
+        """
+        assert 1 == 1
