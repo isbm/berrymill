39a149eb585b65340d36fa631439f3a2586eb52a
Bo Maryniuk <bo@maryniuk.net>
2023-10-11 21:59:59 +0200

    Add UT test_loader_traversal_level_last

--- test/test_traverse.py
+++ test/test_traverse.py
@@ -20,3 +20,21 @@ class TestLoaderTraversal:
         l:XLoader = XLoader()
         l.load("test/chain_a.xml")
         assert l._Loader__i_stack == ['test/test_appliance.xml'], "Traversal path should point to 'test_appliance.xml'"
+
+    def test_loader_traversal_level_last(self):
+        """
+        Load full chain
+        """
+        class XLoader(Loader):
+            def load(self, pth: str) -> str:
+                """
+                Load appliance description
+                """
+                self._traverse(pth)
+                self._flatten()
+
+        l:XLoader = XLoader()
+        l.load("test/chain_d.xml")
+
+        res = ['test/test_appliance.xml', 'test/chain_a.xml', 'test/chain_b.xml', 'test/chain_c.xml']
+        assert l._Loader__i_stack == res, "Traversal path should point to a four documents"
