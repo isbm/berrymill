abb6d9b990c873495ee799c6a765cbaf22d811df
Andre Barthel <Andre.Barthel@elektrobit.com>
2023-10-11 14:12:52 +0200

    Major reworks:     - Improved Parameter parsing     - Added Subparser (build, prepare)     - Support for kiwi-ng system prepare     - Added no-accel for boxbuilding     - Added allow-existing-root for prepare     - minimize reduntant code with better inheritance     - Introduced class KiwiParent for kiwi Repo Handling     - Split classes in differnt files

---
+++ src/berry_mill/preparetask.py
@@ -0,0 +1,43 @@
+from typing import Dict
+from kiwi.tasks.system_prepare import SystemPrepareTask
+import logging
+
+log = logging.getLogger('kiwi')
+
+class PrepareTask(SystemPrepareTask):
+    """
+    Wrapper Class
+    Overloads load_xml_description(...).
+    Method is expanded to directly alter the xml state 
+    in order to inject the in berrymill.conf defined repositories
+
+    """
+    def __init__(self, repos:Dict[str, Dict[str,str]]) -> None:
+        super().__init__()
+        self.repos:Dict[str, Dict[str,str]] = repos
+
+    
+    def load_xml_description(self, description_directory: str, kiwi_file: str = '') -> None:
+        super().load_xml_description(description_directory, kiwi_file)
+        
+        self.xml_state.delete_repository_sections()
+        for reponame in self.repos:
+
+            repodata:Dict[str,str] = self.repos.get(reponame)
+
+            components = repodata.get("components", "/")
+            components = components if components != '/' else None
+            if components:
+                components = components.replace(",", " ")
+            distro = repodata.get("name", None)
+            print(repodata.get("key"))
+            self.xml_state.add_repository(
+                repo_source=repodata.get("url"),
+                repo_type=repodata.get("type"),
+                repo_alias=reponame,
+                repo_signing_keys=[repodata.get("key")],
+                components=components,
+                distribution=distro,
+                repo_gpgcheck=False
+            )
+
