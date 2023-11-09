112d785ee5644167481a0dc181e3623084b77295
Andre Barthel <Andre.Barthel@elektrobit.com>
2023-11-09 10:37:07 +0100

    .gnupg dir for everyone

--- src/berry_mill/kiwiapp.py
+++ src/berry_mill/kiwiapp.py
@@ -22,6 +22,10 @@ class KiwiApp(ABC):
         self._repos = repos
         sys.argv = argv
 
+    def _cechk_gnupg_dir(self) -> None:
+        """creates $HOME/.gnupg directory for gpg key import check"""
+        pathlib.Path().home().joinpath(".gnupg").mkdir(exist_ok=True)
+
     @abstractmethod
     def run(self) -> None:
         """
@@ -34,6 +38,8 @@ class KiwiAppPrepare(KiwiApp):
     Interface between Berrymill and Kiwi-ng Prepare Wrapper
     """
     def run(self) -> None:
+        """create /root/.gnupg if needed and run prepare task"""
+        self._cechk_gnupg_dir()
         PrepareTask(self._repos).process()
 
 
@@ -42,14 +48,8 @@ class KiwiAppLocal(KiwiApp):
     Interface between Berrymill and Kiwi-ng build Wrapper
     """
     def run(self) -> None:
-        """Summary or Description of the Function
-        creates $HOME/.gnupg directory for gpg key import check,
-        and invokes kiwi local build task process
-        """
-        try:
-            pathlib.Path().home().joinpath(".gnupg").mkdir()
-        except FileExistsError:
-            pass
+        """create /root/.gnupg if needed and run local build task process"""
+        self._cechk_gnupg_dir()
         LocalBuildTask(self._repos).process()
 
 
