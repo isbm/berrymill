abb2e7fd01880df3d9d04e70d862629a0354d174
Andre Barthel <Andre.Barthel@elektrobit.com>
2023-11-10 11:01:35 +0100

    rm target dir not writeable test

--- test/test_targetdir.py
+++ test/test_targetdir.py
@@ -1,24 +1,17 @@
 import unittest
 from unittest.mock import patch
+
+from pytest import LogCaptureFixture
+import kiwi.logger
 from berry_mill.builder import KiwiBuilder
 
-class TestTargetDir(unittest.TestCase):
+class TestTargetDir:
 
     @patch("os.path.isdir", lambda f: True)
     @patch("berry_mill.kiwiapp.KiwiAppLocal.run", lambda f: True)
-    def test_dir_exist(self):
-        with self.assertRaises (Exception) as msg:
+    def test_dir_exist(self, caplog: LogCaptureFixture):
+        with caplog.at_level(kiwi.logging.ERROR):
             kb: KiwiBuilder = KiwiBuilder("test/descr/test_appliance.xml", profile="Live", target_dir="/arbitrary/dir")
             kb.process()
-        self.assertTrue("Target directory already exists" in str(msg.exception))
-
-    @patch("os.access")
-    @patch("berry_mill.kiwiapp.KiwiAppLocal.run", lambda f: True)
-    def test_dir_readonly(self, mock_os_access):
-        with self.assertRaises (Exception) as msg:
-            mock_os_access.return_value = False
-            kb: KiwiBuilder = KiwiBuilder("test/descr/test_appliance.xml", profile="Live", target_dir="/arbitrary/dir")
-            kb.process()
-        self.assertTrue("Target directory's parent is not writable" in str(msg.exception))
-
+        assert "Target directory already exists" in caplog.text
         
