5fbb9560e44c03282cb722de39f128178a6c34ec
Bo Maryniuk <bo@maryniuk.net>
2023-11-14 18:28:10 +0100

    Move main() to the package

--- src/berry_mill/__init__.py
+++ src/berry_mill/__init__.py
@@ -1,1 +1,25 @@
 from berry_mill.mill import ImageMill
+
+def main():
+    from berry_mill import ImageMill
+    from typing import Any
+    import sys
+    import kiwi.logger
+
+    log = kiwi.logging.getLogger('kiwi')
+
+    mill:Any = None
+    try:
+        mill = ImageMill()
+    except Exception as exc:
+        log.error("General error:", exc)
+        sys.exit(1)
+
+    try:
+        mill.run()
+    except Exception as exc:
+        log.error("Run error:", exc)
+        if mill.args.debug:
+            raise
+    finally:
+        mill.cleanup()
