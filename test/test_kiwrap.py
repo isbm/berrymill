6a1e536eb0863a2bd2844ee3cca06ca4bd801dc4
Aymen OUERTATANI <Aymen.Ouertatani@elektrobit.com>
2023-11-16 11:47:34 +0100

    Fix assert for log message

--- test/test_kiwrap.py
+++ test/test_kiwrap.py
@@ -185,7 +185,7 @@ class TestCollectionKiwiParent:
             try:
                 KiwiParent_instance._check_repokey(repodata, reponame)
             except SystemExit as s:
-                assert s.args == ("key file path wrong for repository test",)
+                assert s.args == ("Wrong key file path for repository test",)
     
     def test_kiwrap_build_wrong_appliance(self, caplog: LogCaptureFixture):
         """
