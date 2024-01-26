1f2579e74afd34cea87555c4d18d3940f8306c38
Bo Maryniuk <bo@maryniuk.net>
2024-01-26 15:57:47 +0000

    Use ID to avoid hardcodings and mistypes

--- src/berry_mill/plugins/kernkompozzer/__init__.py
+++ src/berry_mill/plugins/kernkompozzer/__init__.py
@@ -5,12 +5,14 @@ class Kernkompozzer(plugin.PluginIf):
     """
     Plugin for Kernkonzept hypervisor
     """
+    ID = "kern-hv"
 
     def setup(self, *args, **kw):
         return super().setup(*args, **kw)
 
     def run(self, cfg:ConfigHandler):
+        print(cfg.config[self.ID])
         print("KKZ")
 
 # Register plugin
-plugin.registry(Kernkompozzer(title="compose images with Kernkonzept hypervisor", name="kern-hv"))
+plugin.registry(Kernkompozzer(title="compose images with Kernkonzept hypervisor", name=Kernkompozzer.ID))
